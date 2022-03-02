154f99b0821ac2c140388bc366da980170581fe8
nikuniku <nikunja_1989@yahoo.com>
2022-03-03 00:35:36 +0600

    change

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,5 @@
 /*
-Author       : Shumon Ahammed
+Author       : Kaiser MOllah
 Template Name: Gitanjali Code Rhythm - Bootstrap Template
 Version      : 1.0
 */
@@ -8,3 +8,19 @@ Version      : 1.0
     "use strict";
 	
 })(jQuery);
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
